57051b2de826575bb3d6bd627c5302a8ab3a099c
XmasRights <chris.fonseka@googlemail.com>
2017-02-27 13:46:28 +0000

    Added a JumpingFrogs.swift file to contain the puzzle logic

---
+++ Jumping Frogs/JumpingFrogs.swift
@@ -0,0 +1,20 @@
+//
+//  JumpingFrogs.swift
+//  Jumping Frogs
+//
+//  Created by Christopher Fonseka on 27/02/2017.
+//  Copyright © 2017 ChristopherFonseka. All rights reserved.
+//
+
+import Foundation
+import SearchTreeKit
+
+struct JumpingFrogs
+{
+    func play()
+    {
+        // TODO
+    }
+
+
+}
